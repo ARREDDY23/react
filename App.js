9e1a6b6610ee33c64241df58c9072c66fd8028ad
ARREDDY23 <134255281+ARREDDY23@users.noreply.github.com>
2023-10-18 14:47:47 +0530

    episode no:1

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("h1",{id:"heading"},"React Headin");
+// ReactDOM.createRoot(document.getElementById("root")).render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
+
+// function App() {
+//     return (
+//     <h1>sdcdc</h1>
+//     )
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
